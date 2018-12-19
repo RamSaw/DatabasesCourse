fe9441b2681dba0d5682e0d743680675fff70e99
RamSaw <mikhail@pravilov.com>
2018-12-19 18:34:26 +0300

    Done 1.

--- CW02/1.sql
+++ CW02/1.sql
@@ -1,3 +1,4 @@
+--почему-то не зашло у робота--
 WITH only_the_same as (
 SELECT plantation.country_id, count(*) as num FROM plantation JOIN americanport ON plantation.port_id = americanport.id WHERE americanport.country_id = plantation.country_id
 GROUP BY plantation.country_id
@@ -6,4 +7,11 @@ SELECT country_id, count(*) as number FROM plantation GROUP BY country_id
 ), countries as (
   SELECT only_the_same.country_id, number, num FROM only_the_same JOIN together ON only_the_same.country_id = together.country_id WHERE together.number = only_the_same.num
 )
-SELECT name FROM americancountry JOIN countries ON countries.country_id = americancountry.id
+SELECT name FROM americancountry JOIN countries ON countries.country_id = americancountry.id;
+
+--зашло у робота--
+SELECT americancountry.name FROM (SELECT americancountry.id FROM americancountry EXCEPT (
+    SELECT plantation.country_id FROM plantation JOIN americanport ON plantation.port_id = americanport.id
+    WHERE americanport.country_id != plantation.country_id
+    GROUP BY plantation.country_id
+)) as true_contries JOIN americancountry ON true_contries.id = americancountry.id;
